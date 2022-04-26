77e86a3a7161562077f1d92e5ab7f2c66292d1d5
EduardoAnzardo <104447964+EduardoAnzardo@users.noreply.github.com>
2022-04-26 20:40:17 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,32 +12,38 @@ public class Main {
         int numero4 = matriz.nextInt();
 
         System.out.println("Matriz original");
-        System.out.println("" +numero1+ " " +numero2+ " "+numero3+" " +numero4+"" );
+        System.out.println("" +numero1+ " " +numero2+ "");
+        System.out.println(""+numero3+" " +numero4+"" );
 
         System.out.println("Selecione um angulo que seja 0, 90, 180, 270 ou 360");
         int rotacao = matriz.nextInt();
 
         if (rotacao == 0) {
-            System.out.println("" +numero1+ " " +numero2+ " "+numero3+" " +numero4+"" ); ; 
+            System.out.println("" +numero1+ " " +numero2+ ""); 
+            System.out.println("" +numero3+ " " +numero4+ ""); 
             } 
             else
 
         if (rotacao == 90) {
-            System.out.println("" +numero4+ " " +numero1+ " "+numero2+" " +numero3+"" ); ; 
+            System.out.println("" +numero3+ " " +numero1+ ""); 
+            System.out.println("" +numero4+ " " +numero2+ ""); 
             } 
             else
 
         if (rotacao == 180) {
-            System.out.println("" +numero3+ " " +numero4+ " "+numero1+" " +numero2+"" ); ;
+            System.out.println("" +numero4+ " " +numero3+ ""); 
+            System.out.println("" +numero2+ " " +numero1+ ""); 
             } 
             else
         if (rotacao == 270) {
-            System.out.println("" +numero2+ " " +numero3+ " "+numero4+" " +numero1+"" );
+            System.out.println("" +numero2+ " " +numero4+ ""); 
+            System.out.println("" +numero1+ " " +numero3+ ""); 
             } 
             else
         if (rotacao == 360) {
-            System.out.println("" +numero1+ " " +numero2+ " "+numero3+" " +numero4+"" ); 
-            }
+            System.out.println("" +numero1+ " " +numero2+ ""); 
+            System.out.println("" +numero3+ " " +numero4+ ""); 
+            } 
             else
         if (rotacao != +0+ +90+ +180+ +270+ +360){
             System.out.println("Esse angulo não foi aceito");
